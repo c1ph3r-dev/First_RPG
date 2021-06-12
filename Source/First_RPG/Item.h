745a25744e0aa47778d42008861f5d09e1f14192
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2021-06-12 08:23:46 -0400

    Added Base Item class

---
+++ Source/First_RPG/Item.h
@@ -0,0 +1,33 @@
+// Copyright Jonathan Justin Rampersad 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class FIRST_RPG_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Item | Collision")
+	class USphereComponent* CollisionVolume;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
